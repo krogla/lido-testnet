c19b50041e3c283297048f6c10be6f000e078d76
KRogLA <i@krogla.me>
2021-03-31 12:43:25 +0200

    feat: custom local port

--- 3.run_validators.sh
+++ 3.run_validators.sh
@@ -4,12 +4,16 @@ source ./.env
 set -e +u
 set -o pipefail
 
-docker run -d --rm -v "$(pwd):/data" -p "127.0.0.1:5062:5062" sigp/lighthouse lighthouse \
+:${PORT:=5062}
+:${USER:=default}
+
+docker run -d --rm -v "$(pwd):/data" -p "127.0.0.1:$PORT:5062" sigp/lighthouse lighthouse \
     validator_client \
     --http \
     --network prater \
     --init-slashing-protection \
     --datadir "/data" \
     --beacon-nodes $BEACONNODE \
-    --graffiti $GRAFFITI
+    --name validator-$USER
+    --graffiti "$GRAFFITI"
 
