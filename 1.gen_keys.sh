708cbe9c748c0e4fa7e507c3b2d0d34c8103fada
KRogLA <i@krogla.me>
2021-08-23 20:32:56 +0200

    add validator index and count env

--- 1.gen_keys.sh
+++ 1.gen_keys.sh
@@ -5,26 +5,28 @@ set -e +u
 set -o pipefail
 
 PASSWORD=$(head -n 1 ./validators.pass)
+START_INDEX=${START_INDEX:-0}
+VALIDATORS_COUNT=${VALIDATORS_COUNT:-3}
 
 if [ -z "$MNEMONIC" ]; then
     docker run -it --rm --pull "always" \
-        -v "$(pwd)/validator_keys:/data" \
+        -v "$(pwd):/data" \
         lidofinance/deposit-cli:latest new-mnemonic \
         --chain=prater \
         --quiet \
-        --num_validators=3 \
+        --num_validators=$VALIDATORS_COUNT \
         --folder=/data \
         --keystore_password=$PASSWORD \
         --eth1_withdrawal_address=$WITHDRAWAL_ADDRESS \
         --mnemonic_language=english
 else
     docker run -it --rm --pull "always" \
-        -v "$(pwd)/validator_keys:/data" \
+        -v "$(pwd):/data" \
         lidofinance/deposit-cli:latest existing-mnemonic \
         --chain=prater \
         --quiet \
-        --validator_start_index=0 \
-        --num_validators=3 \
+        --validator_start_index=$START_INDEX \
+        --num_validators=$VALIDATORS_COUNT \
         --folder=/data \
         --keystore_password=$PASSWORD \
         --eth1_withdrawal_address=$WITHDRAWAL_ADDRESS \
